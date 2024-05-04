89685369511d86b76fa5d23a8c3b2602e57b7406
audalio-devops <audalio.devops@gmail.com>
2024-05-04 13:50:45 -0300

    Inclusao (post) implementada

--- src/main/java/com/ajrdevops/crudspring/controller/CourseController.java
+++ src/main/java/com/ajrdevops/crudspring/controller/CourseController.java
@@ -1,14 +1,21 @@
 package com.ajrdevops.crudspring.controller;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ajrdevops.crudspring.model.Course;
 import com.ajrdevops.crudspring.repository.CourseRepository;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/api/courses")
@@ -23,5 +30,17 @@ public class CourseController {
 
         return courseRepository.findAll();
     }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        //System.out.println(course);
+        return courseRepository.save(course);
+    
+        //public ResponseEntity<Course> create(@RequestBody Course course){
+        //return ResponseEntity.status(HttpStatus.CREATED)
+        //                     .body(course);}
+    }
+    
     
 }
