374a9923fa6c11e9c4a63ed1831aa7f619ae3843
audalio-devops <audalio.devops@gmail.com>
2024-05-05 23:55:16 -0300

    Implementacao do findById

--- src/main/java/com/ajrdevops/crudspring/controller/CourseController.java
+++ src/main/java/com/ajrdevops/crudspring/controller/CourseController.java
@@ -1,10 +1,11 @@
 package com.ajrdevops.crudspring.controller;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,8 @@ import com.ajrdevops.crudspring.repository.CourseRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -30,6 +33,16 @@ public class CourseController {
 
         return courseRepository.findAll();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id) {
+
+        return courseRepository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound()
+                .build());
+    }
+    
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
